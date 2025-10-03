a0e640d256792e6116c1dca15f38a5f2371af910
Affan-Rafique <affanrafique9@gmail.com>
2025-10-03 16:13:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+const form = document.getElementById("form");
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // stop form from reloading page
+
+  const nname = nameInput.value.trim();
+  const aage = ageInput.value.trim();
+
+  // Validation
+  if (!nname || !aage) {
+    alert("Please Enter Valid Details");
+    return;
+  }
+
+  // Promise
+  const checkAge = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(aage) > 18) {
+        resolve(`Welcome, ${nname}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${nname}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  // Handle promise
+  checkAge
+    .then((msg) => alert(msg))
+    .catch((err) => alert(err));
+});
+
+
+
+
+	 
